b4c699f81a871a3ea4723685fe099a1c091f0130
salog0d <david.salomon.nava11@gmail.com>
2025-12-02 09:39:49 -0600

    app entry point

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
-from src.core.settings import settings
-from fastapi import Fastapi
+import uvicorn
 
-app = Fastapi()
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.app:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+    )
